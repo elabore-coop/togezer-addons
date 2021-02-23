7ae5bc8b1e7dafc898fe76b89b59559c47b6eb71
Stéphan Sainléger <contact@stephansainleger.fr>
2021-02-23 16:16:02 +0100

    Migration to v13

--- partner_gogocarto_export_api/__manifest__.py
+++ partner_gogocarto_export_api/__manifest__.py
@@ -8,7 +8,7 @@
     ),
     'website': 'https://lokavaluto.fr',
     'category': 'Localization',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': [
         'base',
         'contacts',
