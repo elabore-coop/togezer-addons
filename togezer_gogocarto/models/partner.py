e82a4323d86f252ed8498fd630be4d55d6ec2e6e
clementthomas <clement@cotyl.net>
2023-05-04 13:38:51 +0200

    [IMP] togezer_gogocarto: add touristic tours and gallery to gogo api

--- togezer_gogocarto/models/partner.py
+++ togezer_gogocarto/models/partner.py
@@ -5,8 +5,31 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     def _get_gogocarto_parser(self, company_id):
-        parser = super(Partner,self)._get_gogocarto_parser(company_id)
-        for field in parser:
-            if type(field) is tuple and field[0] == 'child_ids':
-                field[1].append('website_short_description')
+        parser = []
+        for field in self._get_export_fields(company_id):
+            if field.ttype in [
+                    "boolean",
+                    "char",
+                    "integer",
+                    "monetary",
+                    "text",
+                    "selection",
+                    "float",
+                    "date_time",
+                    "date"]:
+                parser.append(field.name)
+            elif field.name == 'child_ids':
+                parser.append((field.name, ['id', 'name','website_short_description']))
+            elif field.name == 'x_pictures':
+                parser.append((field.name, ['id', 'x_url']))
+            elif field.name == 'x_touristic_tours':
+                parser.append((field.name, ['id', 'x_name', 'x_url']))
+            elif field.ttype in ["many2one", "one2many", "many2many"]:
+                parser.append((field.name, ['id', 'name']))
+            elif field.ttype == "binary":
+                continue
+            elif field.ttype == "html":
+                continue  # Not developped so far
+            else:
+                continue
         return parser
